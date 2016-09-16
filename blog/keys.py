54aa08985a6e70cfaf707b99cde45165b8ae0b22
frankmolotov <molkonigo@gmail.com>
2016-09-16 16:45:44 +0300

    added google analytics

--- blog/keys.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def get_encrypted_key(key):
-    file = open(key, 'r')
-    information = file.readlines()
-    return information
-
-key = 'test.txt'
-print(get_encrypted_key(key))
